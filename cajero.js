53b6a028764ef241286ec52092776e5a78242b5a
RBalleste <rballeste69@gmail.com>
2022-02-23 11:05:04 +0100

    Afegeixo css
    I primer intent de veure els bitllets

--- cajero.js
+++ cajero.js
@@ -1,31 +1,66 @@
 class Billete
 {
-    constructor(v,c)
-    {
-        this.valor=v;
-        this.cantidad=c;
-    }
+  constructor(v, c)
+  {
+    this.valor = v;
+    this.cantidad = c;
+  }
 }
+
 function entregarDinero()
 {
-    for(var bi of caja)
+  var t = document.getElementById("dinero");
+  dinero = parseInt(t.value);
+
+  for(var bi of caja)
+  {
+
+    if(dinero > 0)
     {
-      if(dinero>0)
+      div = Math.floor(dinero / bi.valor);
+
+      if(div > bi.cantidad)
       {
-          div = Math.floor(dinero/ bi.valor);
-      }  
+        papeles = bi.cantidad;
+      }
+      else
+      {
+        papeles = div;
+      }
+
+      entregado.push( new Billete(bi.valor, papeles) );
+      dinero = dinero - (bi.valor * papeles);
     }
+
+  }
+  
+  if(dinero > 0)
+  {
+    resultado.innerHTML = "No puedo darte esa cantidad.<br/>";
+  }
+  else
+  {
+    for(var e of entregado)
+    {
+      if(e.cantidad > 0)
+      {
+              resultado.innerHTML += e.cantidad + " billetes de " + e.valor + " €.<br />";
+      }
+    }
+  }
 }
 
-var caja=[];
-var entregado=[];
-caja.push( new Billete(50,3));
-caja.push( new Billete(20,2));
-caja.push( new Billete(10,2));
+var caja = [];
+var entregado = [];
+caja.push( new Billete(100, 5) );
+caja.push( new Billete(50, 10) );
+caja.push( new Billete(20, 5) );
+caja.push( new Billete(10, 10) );
+caja.push( new Billete(5, 5) );
+var dinero = 0;
+var div = 0;
+var papeles = 0;
 
-var dinero=210;
-var dir=0;
-var papeles=0;
-
-var b= document.getElementsById(extraer)
-document.addEventListener("click", entregarDinero)
+var resultado = document.getElementById("resultado");
+var b = document.getElementById("extraer");
+b.addEventListener("click", entregarDinero);
